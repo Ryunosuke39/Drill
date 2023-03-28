04c650e6f52162a43747b267f85652d89a4f8436
Ryunosuke39 <111721351+Ryunosuke39@users.noreply.github.com>
2023-03-28 14:34:25 +1100

    terraingen added

--- Drill/Assets/Scripts/TerrarianGeneration.cs
+++ Drill/Assets/Scripts/TerrarianGeneration.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class TerrarianGeneration : MonoBehaviour
 {
-    //How to make a 2d game like terraira
+    //YouTube: How to make a 2d game like terraira
+    public Sprite tile;
     public int worldSize = 100;
     public float noiseFreq = 0.05f;
-    public float speed;
+    public float seed;
     public Texture2D noiseTexture;
 
     private void Start()
     {
+        seed = Random.Range(-1000, 1000);
         GenerateNoiseTexture();
+        GenerateTerrain();
     }
 
-    private void GenerateNoiseTexture()
+    public void GenerateTerrain()
     {
 
+        for (int x = 0; x < worldSize; x++)
+        {
+            for (int y = 0; y < worldSize; y++)
+            {
+                GameObject newTile = new GameObject();
+                newTile.AddComponent<SpriteRenderer>();
+                newTile.GetComponent<SpriteRenderer>().sprite = tile;
+                newTile.transform.position = new Vector2(x, y);
+            }
+        }
+    }
+
+    public void GenerateNoiseTexture()
+    {
+        noiseTexture = new Texture2D(worldSize, worldSize);
+
+        for(int x = 0; x < noiseTexture.width; x++)
+        {
+            for (int y = 0; y < noiseTexture.height; y++)
+            {
+                float v = Mathf.PerlinNoise((x + seed) * noiseFreq, (y + seed) * noiseFreq);
+                noiseTexture.SetPixel(x, y, new Color(v, v, v));
+            }
+        }
+
+        noiseTexture.Apply();
     }
 
 }
